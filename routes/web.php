736776b6cff3ca01eb85176234f7c77f4cee80bd
vcttai <vcttai@gmail.com>
2019-08-25 17:19:53 +0700

    revert routing

--- routes/web.php
+++ routes/web.php
@@ -15,35 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function() {
-    $name = "vcttai";
-    $web = "https://nhungdongcodevui.com";
-
-    return view('about', [
-        'view_name' => $name,
-        'view_web' => $web
-    ]);
-});
-
-Route::get('home', function() {
-    return view('home');
-});
-
-//ROUTES OF POSTS
-Route::get('/posts', 'PostController@index');
-
-Route::get('/post_detail/{id}', 'PostController@show');
-
-
-//ROUTES OF blade
-Route::get('/test_master_layout', function(){
-    return view('layouts.master');
-});
-
-Route::get('/child_page_1', function(){
-    return view('layouts.child_page_1');
-});
-
 // AUTH
 Auth::routes();
 
