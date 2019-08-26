dbebd88f8ddcbf22462e270e118a91da1e9fc360
TranGunner <46949123+TranGunner@users.noreply.github.com>
2019-08-26 22:58:59 +0700

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,8 @@
     <title> @yield('title', 'Online Shopping')</title>
     <link rel="stylesheet" type="text/css" href="{{asset('css/memenu.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 
