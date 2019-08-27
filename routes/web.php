3730fbd37a79db0d7b077e7da8b61f49ca781292
TranThuyDung <52059633+TranThuyDung@users.noreply.github.com>
2019-08-27 08:38:42 +0700

    commit ui_cart

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/cart', 'CartController@index');
+
+Route::get('/check_out', 'CheckOutController@index');
