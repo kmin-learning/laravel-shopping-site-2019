fbf928851a41bb33eb6eed23e45c48bda5a1ece7
pvtot <phamvantot1999@gmail.com>
2019-09-15 21:07:51 +0700

    model + migration (products + orders)

--- app/Product.php
+++ app/Product.php
@@ -8,4 +8,9 @@ class Product extends Model
 {
     //
     public $timestamp = false;
+    protected $table = 'products';
+
+    public function orders(){
+        return $this->belongsToMany('App\order','order_product','order_id','product_id')->withPivot('quantity','total');
+    }
 }
